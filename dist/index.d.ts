0c0c6c67c07df0433bd6e87ca5040471601aa500
Tony <toneyobaze@gmail.com>
2021-07-09 10:13:43 +0000

    add github repo url to package.json

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,11 +1,12 @@
 import React from "react";
-import { PressableProps } from "react-native";
+import { PressableProps, ViewStyle } from "react-native";
 interface Props extends PressableProps {
     onPress?: () => {};
     children?: React.ReactNode;
     scaleValue?: number;
     friction?: number;
     tension?: number;
+    style?: ViewStyle | ViewStyle[];
 }
-declare const RNTAnimatedPressable: ({ children, onPress, scaleValue, friction, tension, ...restProps }: Props) => JSX.Element;
+declare const RNTAnimatedPressable: ({ children, onPress, scaleValue, friction, tension, style, ...restProps }: Props) => JSX.Element;
 export default RNTAnimatedPressable;
